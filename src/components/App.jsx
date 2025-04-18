496d5d106147a171cfc7e7beb1d1127a52c64316
Raavi Kaur <raavi211299@gmail.com>
2025-04-17 22:16:20 -0700

    implement the event on the button and change the color pf button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <form>
-        <h1>Hello</h1>
-        <input type="text" placeholder="What is your first name?" />
-        <button type="submit">Submit</button>
-        </form>
-    </div>
-  );
+    const [isMouseOver, setMouseOver] = useState(false);
+    function handleMouseOver() {
+        setMouseOver(true);
+      }
+      function handleMouseRelease() {
+        setMouseOver(false);
+      }
+    return (
+        <div className="container">
+            <form>
+                <h1>Hello</h1>
+                <input type="text" placeholder="What is your first name?" />
+                <button style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseRelease}
+                type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
