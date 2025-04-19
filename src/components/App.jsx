3048f5827a99f9713317bf551eda29d094f0c9a5
Raavi Kaur <raavi211299@gmail.com>
2025-04-18 22:27:02 -0700

    add the function to handle changes in input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 
 function App() {
     const [isMouseOver, setMouseOver] = useState(false);
+    const [fnameText, setFname] = useState("");
+
     function handleMouseOver() {
         setMouseOver(true);
-      }
-      function handleMouseRelease() {
+    }
+    function handleMouseRelease() {
         setMouseOver(false);
-      }
+    }
+    function handleChange(event) {
+        setFname(event.target.value);
+    }
     return (
         <div className="container">
             <form>
-                <h1>Hello</h1>
-                <input type="text" placeholder="What is your first name?" />
+                <h1>Hello {fnameText}</h1>
+                <input onChange={handleChange} type="text" placeholder="What is your first name?" />
                 <button style={{ backgroundColor: isMouseOver ? "black" : "white" }}
                 onMouseOver={handleMouseOver}
                 onMouseOut={handleMouseRelease}
