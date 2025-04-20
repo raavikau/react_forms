a2a68ad9a538e184f6a5ccb60ce261ecabd039c9
Raavi Kaur <raavi211299@gmail.com>
2025-04-19 23:10:38 -0700

    optimize the code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,21 +7,22 @@ function App() {
     function handleMouseOver() {
         setMouseOver(true);
     }
+
     function handleMouseRelease() {
         setMouseOver(false);
     }
+
     function handleChange(event) {
         setFname(event.target.value);
     }
+
     return (
         <div className="container">
             <form>
                 <h1>Hello {fnameText}</h1>
                 <input onChange={handleChange} type="text" placeholder="What is your first name?" />
-                <button style={{ backgroundColor: isMouseOver ? "black" : "white" }}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseRelease}
-                type="submit">
+                <button style = {{ backgroundColor: isMouseOver ? "black" : "white" }}
+                onMouseOver={handleMouseOver} onMouseOut={handleMouseRelease} type="submit">
                     Submit
                 </button>
             </form>
