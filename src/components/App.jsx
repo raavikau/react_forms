d92f0fa4fe5e792d4a52772d99993548cd236c00
Raavi Kaur <raavi211299@gmail.com>
2025-04-25 21:55:38 -0700

    implement the code on submit button display text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 function App() {
     const [isMouseOver, setMouseOver] = useState(false);
     const [fnameText, setFname] = useState("");
-
+    const [headingText, setHeading] = useState("");
+    
     function handleMouseOver() {
         setMouseOver(true);
     }
@@ -16,11 +17,16 @@ function App() {
         setFname(event.target.value);
     }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        setHeading(fnameText);
+    }
+
     return (
         <div className="container">
-            <form>
-                <h1>Hello {fnameText}</h1>
-                <input onChange={handleChange} type="text" placeholder="What is your first name?" />
+            <form onSubmit={handleSubmit}>
+                <h1>Hello {headingText}</h1>
+                <input onChange={handleChange} type="text" placeholder="What is your first name?" value={fnameText}/>
                 <button style = {{ backgroundColor: isMouseOver ? "black" : "white" }}
                 onMouseOver={handleMouseOver} onMouseOut={handleMouseRelease} type="submit">
                     Submit
