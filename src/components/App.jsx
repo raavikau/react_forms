371973652b5d9a3af33db7b1691229c32bbf4b11
Raavi Kaur <raavi211299@gmail.com>
2025-04-27 23:19:43 -0700

    add Event component to handle events in form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,10 @@
 import React, { useState } from "react";
-
+import Event from "./Event";
+ 
 function App() {
-    const [isMouseOver, setMouseOver] = useState(false);
-    const [fnameText, setFname] = useState("");
-    const [headingText, setHeading] = useState("");
-    
-    function handleMouseOver() {
-        setMouseOver(true);
-    }
-
-    function handleMouseRelease() {
-        setMouseOver(false);
-    }
-
-    function handleChange(event) {
-        setFname(event.target.value);
-    }
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        setHeading(fnameText);
-    }
-
     return (
         <div className="container">
-            <form onSubmit={handleSubmit}>
-                <h1>Hello {headingText}</h1>
-                <input onChange={handleChange} type="text" placeholder="What is your first name?" value={fnameText}/>
-                <button style = {{ backgroundColor: isMouseOver ? "black" : "white" }}
-                onMouseOver={handleMouseOver} onMouseOut={handleMouseRelease} type="submit">
-                    Submit
-                </button>
-            </form>
+            <Event />
         </div>
     );
 }
---
+++ src/components/Event.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function Event() {
+    const [isMouseOver, setMouseOver] = useState(false);
+    const [fnameText, setFname] = useState("");
+    const [headingText, setHeading] = useState("");
+
+    function handleMouseOver() {
+        setMouseOver(true);
+    }
+
+    function handleMouseRelease() {
+        setMouseOver(false);
+    }
+
+    function handleChange(event) {
+        setFname(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setHeading(fnameText);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Hello {headingText}</h1>
+            <input onChange={handleChange} type="text" placeholder="What is your first name?" value={fnameText}/>
+            <button 
+                style = {{ backgroundColor: isMouseOver ? "black" : "white" }}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseRelease} 
+                type="submit">
+                Submit
+            </button>
+        </form>
+    );
+}
+
+export default Event;
