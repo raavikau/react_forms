0d14bac068395ade9e4580f6c51e4892bd36dc79
Raavi Kaur <raavi211299@gmail.com>
2025-06-14 22:15:15 -0700

    optimize the code ES6 code with spread operator'

--- src/components/ComplexState.jsx
+++ src/components/ComplexState.jsx
@@ -1,45 +1,49 @@
 import React, { useState } from "react";
 
 function ComplexState() {
-    const [setContact, submitContact] = useState({fname:"", lname:"", email:""});
+    const [contact, setContact] = useState({ fname: "", lname: "", email: "" });
     function handleChange(event) {
-        const {name, value} = event.target;
-        // submitContact((previous)=>{
-        //     if (inputName === "fname"){
+        const {name, value} = event.target; // its equal to event.target.name
+        // setContact((previous)=>{
+        //   // function with pasing previous val var
+        //     if (name === "fname"){
         //         return {
-        //             fname:newValue,
+        //             fname:value,
         //             lname:previous.lname,
         //             email:previous.email
         //         };
-        //     } else if (inputName === "lname") {
+        //     } else if (name === "lname") {
         //         return {
         //             fname:previous.fname,
-        //             lname:newValue,
+        //             lname:value,
         //             email:previous.email
         //         };
-        //     } else if (inputName === "email") {
+        //     } else if (name === "email") {
         //         return {
         //             fname:previous.fname,
         //             lname:previous.lname,
-        //             email:newValue
+        //             email:value
         //         };
         //     }
         // });
 
-        // you can use this ES6 optimize code instead of above submitContact function
-        submitContact((previousvalue)=> {
-            return { ...previousvalue, [name]:value };
+        // you can use this ES6 optimize code with spread operator instead of above submitContact function
+        setContact((previousvalue)=> {  //give all previous values // key as [name]
+            return { 
+                ...previousvalue,
+                 [name]:value
+            };
         });
         
     }
     return (
         <>
-            <h1>Hello {setContact.fname} {setContact.lname}</h1>
-            <p>Email {setContact.email}</p>
+            <h1>Hello {contact.fname} {contact.lname}</h1>
+            <p>{contact.email}</p>
             <form>
-                <input onChange={handleChange} name="fname" placeholder="First Name" value={setContact.fname} />
-                <input onChange={handleChange} name="lname" placeholder="Last Name" value={setContact.lname} />
-                <input onChange={handleChange} name="email" placeholder="Email" value={setContact.email} />
+                <input onChange={handleChange} name="fname" placeholder="First Name" value={contact.fname} />
+                <input onChange={handleChange} name="lname" placeholder="Last Name" value={contact.lname} />
+                <input onChange={handleChange} name="email" placeholder="Email" value={contact.email} />
                 <button type="submit">Submit</button>
             </form>
         </>
